c7a7cb5dcc1fbaa4530e1173caaf898e5e4d84d1
YangHuijin1112 <yangjinee@naver.com>
2020-08-17 16:15:02 +0900

    Updates

---
+++ precache-manifest.6bb2192798e49ff577a5a2129284819b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd234d41a8aa8b08eb1d8a62057f3cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf2cc5b06e9d538e5983",
+    "url": "/static/css/main.8e92de87.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2cc5b06e9d538e5983",
+    "url": "/static/js/main.2389ec6d.chunk.js"
+  },
+  {
+    "revision": "cf27343b5d386281fb60",
+    "url": "/static/js/runtime-main.1e9c0366.js"
+  }
+]);
